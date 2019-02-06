fd51f0134619d9bd38e6238d22fc4cb3c4d21f52
cedric-ritaly9 <cedric.ritaly@gmail.com>
2019-02-07 00:14:29 +0100

    ex1

--- src/main/java/fr/umontpellier/iut/exercice1/App.java
+++ src/main/java/fr/umontpellier/iut/exercice1/App.java
@@ -1,9 +1,20 @@
 package fr.umontpellier.iut.exercice1;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        throw new RuntimeException("Not yet implemented !");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Veuillez saisir un nombre :");
+        int str = sc.nextInt();
+
+        FizzBuzz fizzbuzz=new FizzBuzz();
+        fizzbuzz.getValue(str);
+        fizzbuzz.computeList(str);
     }
 
 }
+
+
--- src/main/java/fr/umontpellier/iut/exercice1/FizzBuzz.java
+++ src/main/java/fr/umontpellier/iut/exercice1/FizzBuzz.java
@@ -2,11 +2,34 @@ package fr.umontpellier.iut.exercice1;
 
 public class FizzBuzz {
 
+
+    FizzBuzz() {}
+
     public String getValue(int i) {
-        throw new RuntimeException("Not yet implemented !");
+        System.out.println("Fizz Buzz vaut : "+i+"\n");
+        return ("Fizz Buzz vaut : "+i+"\n") ;
     }
 
+
     public String[] computeList(int i) {
-        throw new RuntimeException("Not yet implemented !");
+        i++;
+        String[] s= new String[i];
+
+        int paopti=i-1;
+
+
+        for (int j=1;j<i;j++)
+        {
+            s[j]="";
+            if (j%3==0) s[j]="Fizz";
+            if (j%5==0) s[j]+="Buzz";
+            if (!(j%3==0) ) {if (!(j%5==0)) s[j]+=Integer.toString(j);}
+            if (!(j==paopti)) s[j]+=" ; ";
+
+            System.out.print(s[j]);
+        }
+
+
+        return s;
     }
 }
